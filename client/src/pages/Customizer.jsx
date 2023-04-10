007a63a6f9576f17b5ea2dd931bd92558be3d0e6
choiruladamm <choiruladamm@gmail.com>
2023-04-11 00:02:38 +0700

    ThreeJS

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -15,17 +15,34 @@ import {
   Tab,
 } from "../components";
 
-
 const Customizer = () => {
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
 
-  return <AnimatePresence>
-    {!snap.intro && (
-      <>
-        Customizer
-      </>
-    )}
-  </AnimatePresence>;
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div>
+            
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
