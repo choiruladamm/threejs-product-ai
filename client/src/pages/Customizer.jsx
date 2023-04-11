b488f07f85303241f3c09eeca5afae38c625a44c
ethanlionel <giginetpojok@gmail.com>
2023-04-11 13:38:00 +0700

    Layouting Home, Customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -36,8 +36,28 @@ const Customizer = () => {
             </div>
           </motion.div>
 
-          <motion.div>
-            
+          <motion.div className="absolute top-5 right-5" {...fadeAnimation}>
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 px-2.5 font-bold text-sm"
+            />
+          </motion.div>
+
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab
+                handleClick={() => {}}
+              />
+            ))}
           </motion.div>
         </>
       )}
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from "valtio";
 
 const state = proxy({
   intro: true,
-  color: "#EFBD48",
+  color: "#3A98B9",
   isLogoTexture: true,
   isFullTexture: false,
   logoDecal: "./threejs.png",
